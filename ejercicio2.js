ae9b7a5a92776a26ac9cfa397ee587111a30a27d
Karen Rivera <karenriverah.18@gmail.com>
2022-11-17 20:39:57 -0500

    editar

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+/*let pasaraFahrenheit = (grado) => {return Fahrenheit = grado * 1.8 + 32;}
+
+let result = pasaraFahrenheit(22);
+alert(result);*/
+
+let pasaraCelsius = (grado) => {return Celsius = (grado - 32) / 1.8;}
+
+let result = pasaraCelsius(150);
+alert(result);
---
+++ ejercicio3.js
@@ -0,0 +1,10 @@
+
+let num1 =+prompt ("Introduce un número")
+let num2 =+prompt ("Introduce otro número")
+
+function suma(num1,num2){
+    return num1+num2
+};
+const result = suma(num1,num2);
+alert(result);
+
